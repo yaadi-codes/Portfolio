7af1e3cde5c74a669c0fa3af20fb10a4a423e8b5
yaadi-codes <maliquejoel333@gmail.com>
2025-12-24 00:44:16 -0500

    commit

--- src/Components/Home_Page/HomePage.tsx
+++ src/Components/Home_Page/HomePage.tsx
@@ -3,7 +3,6 @@ import ScrollCue from "../ScrollCue";
 import Star from "../Star";
 import "../../Styles/homePage.css";
 import HomeText from "./HomeText";
-import githubPfp from "../../assets/githubPfp.png";
 
 const HomePage = () => {
   return (
@@ -19,7 +18,7 @@ const HomePage = () => {
             rel="noopener noreferrer"
           >
             <img
-              src={githubPfp}
+              src="/githubPfp.png"
               alt="Profile Picture"
               id="profile-pic"
               title="Click to visit my GitHub Account 👨‍💻."
